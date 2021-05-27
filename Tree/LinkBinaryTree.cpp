621db6fef317ca93ff55bb42dbd41d4427d002f3
TheMostCai <1135279795@qq.com>
2021-05-27 21:58:14 +0800

    新增二叉树层序遍历

--- Tree/LinkBinaryTree.cpp
+++ Tree/LinkBinaryTree.cpp
@@ -50,3 +50,49 @@ int visit(BiTNode* P) {
     cout << P->data << " " << endl;
     return P->data;
 }
+
+//求二叉树深度
+int treeDepth(BiTree T) {
+    if (T == NULL) {
+        return 0;
+    } else {
+        int l = treeDepth(T->lChild);
+        int r = treeDepth(T->rChild);
+        //树的深度=max{左子树深度,右子树深度}+1
+        return l > r ? l + 1 : r + 1;
+    }
+}
+
+//定义层次遍历中所需的队列结点
+typedef struct QNode {
+    BiTree* data;
+    QNode* nexe;
+} QNode;
+
+//定义层次遍历中所需要的队列
+typedef struct {
+    QNode *front, *rear;
+} LinkQueue;
+
+//层序遍历树
+void LevelOrder(BiTree T) {
+    LinkQueue Q;
+    //初始化Q
+    // InitQueue(Q);
+    BiTree p;
+    //向队列中插入根节点
+    // EnQueue(Q,T)
+    while (1) {  //队列非空
+        //根节点出队
+        // DeQueue(Q, p);
+        visit(p);
+        if (p->lChild != NULL) {
+            //左孩子结点入队
+            // EnQueue(Q,p->lChild);
+        }
+        if (p->rChild != NULL) {
+            //右孩子入队
+            // EnQueue(Q,p->rChild);
+        }
+    }
+}
